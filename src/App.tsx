ef5059cdcfab954b78452a6bab5df9d3f49b8b47
Vastille Dolcine <vdolcine@gmail.com>
2025-05-07 22:02:37 -0700

    control red square with arrow keys

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,56 @@
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+	const [position, setPosition] = useState({ x: 250, y: 250 });
+	const squareSize = 20;
+
+	const handleKeyDown = (event: KeyboardEvent) => {
+		switch (event.key) {
+			case 'ArrowLeft':
+				setPosition((prevPosition) => ({ ...prevPosition, x: prevPosition.x - 10 }));
+				break;
+			case 'ArrowRight':
+				setPosition((prevPosition) => ({ ...prevPosition, x: prevPosition.x + 10 }));
+				break;
+			case 'ArrowUp':
+				setPosition((prevPosition) => ({ ...prevPosition, y: prevPosition.y - 10 }));
+				break;
+			case 'ArrowDown':
+				setPosition((prevPosition) => ({ ...prevPosition, y: prevPosition.y + 10 }));
+				break;
+			default:
+				break;
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener('keydown', handleKeyDown);
+
+		return () => {
+			window.removeEventListener('keydown', handleKeyDown);
+		};
+	}, []);
+
 	return (
-		<>
-			<h1>Is this a game yet?</h1>
-		</>
+		<div
+			style={{
+				height: '100vh',
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}
+		>
+			<div
+				style={{
+					width: `${squareSize}px`,
+					height: `${squareSize}px`,
+					backgroundColor: 'red',
+					position: 'absolute',
+					left: position.x,
+					top: position.y,
+				}}
+			/>
+		</div>
 	);
 }
 
